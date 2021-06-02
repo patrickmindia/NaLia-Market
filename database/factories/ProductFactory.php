ece9516e50cb2e8ed4ee62f10f31c49ad6aff6d7
Patrick Mindia <patrickmindia@gmail.com>
2021-06-02 18:14:48 +0100

    Factory part 1

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(3, true),
+            'price' => $this->faker->numberBetween(10000, 200000)(),
+            'description' => $this->faker->sentence(10, true)
+        ];
+    }
+}
--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -5,17 +5,15 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductsSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Product::factory(50)->create();
-
+        
     }
 }
